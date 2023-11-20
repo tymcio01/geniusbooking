da3d785eb516ecdec97a6571f610e44af7939ecb
tymcio01 <149621249+tymcio01@users.noreply.github.com>
2023-11-20 16:58:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let calendarDays = document.getElementById('calendarDays');
+    const calendar = document.getElementById('calendar');
+    const prevMonth = document.getElementById('prevMonth');
+    const nextMonth = document.getElementById('nextMonth');
     let currentDate = new Date();
-    
-    function generateCalendarDays() {
-        calendarDays.innerHTML = ''; // Clear current calendar days
+
+    function generateCalendar() {
+        calendar.innerHTML = ''; // Clear the calendar
+        let startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
+
         for (let i = 0; i < 30; i++) {
-            let day = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + i);
+            let day = new Date(startDate);
+            day.setDate(day.getDate() + i);
+
             let dayElement = document.createElement('div');
             dayElement.className = 'day';
-            dayElement.textContent = day.toDateString();
-            calendarDays.appendChild(dayElement);
+            dayElement.textContent = day.getDate();
+            calendar.appendChild(dayElement);
         }
     }
-    
-    // Event listeners for navigation buttons
-    document.getElementById('prevMonth').addEventListener('click', function() {
-        currentDate.setDate(currentDate.getDate() - 30);
-        generateCalendarDays();
-    });
 
-    document.getElementById('nextMonth').addEventListener('click', function() {
-        currentDate.setDate(currentDate.getDate() +
+    prevMonth.onclick = function() {
+        currentDate.setDate(currentDate.getDate() - 30);
+        generateCalendar();
+    };
+
+    nextMonth.onclick = function() {
+        currentDate.setDate(currentDate.getDate() + 30);
+        generateCalendar();
+    };
+
+    generateCalendar(); // Initial call to display the calendar
+});
